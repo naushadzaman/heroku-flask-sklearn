33219dedb41d2913766cca2bf9af60d8a88eb740
Naushad UzZaman <naushadzaman@gmail.com>
2014-02-26 01:52:43 -0500

    added deployment script

---
+++ deployment.sh
@@ -0,0 +1,30 @@
+# setup pre-requisites
+#curl https://raw.github.com/naushadzaman/heroku-flask-sklearn/master/setup_prerequisite.sh | sh
+
+git clone https://github.com/naushadzaman/heroku-flask-sklearn
+
+mkdir sample-foo
+cd sample-foo/
+
+cp ../heroku-flask-sklearn/.gitignore .
+cp ../heroku-flask-sklearn/Procfile .
+cp ../heroku-flask-sklearn/hello.py .
+
+rm -rf ../heroku-flask-sklearn
+
+heroku login
+
+# create heroku buildpack with sklearn and its pre-requisites
+git init
+heroku create --buildpack https://github.com/naushadzaman/heroku-buildpack-python-sklearn/
+
+git add .
+git commit -m 'initialize'
+
+foreman start
+#ctrl + C
+
+git push heroku master
+
+#heroku scale web=1
+#heroku ps
